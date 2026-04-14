cff957e155d9813d5538253f04bfc16a3e7ec766
javinukem <coverodecox@gmail.com>
2026-04-14 15:57:19 +0200

    changes to the problems and plotting script for mhd_blast

--- arena/arena_tests/solver_in_the_loop/multiproblem/problems/mhd_blast.py
+++ arena/arena_tests/solver_in_the_loop/multiproblem/problems/mhd_blast.py
@@ -1,3 +1,13 @@
+if __name__ == "__main__":
+    # from autocvd import autocvd
+    #
+    # autocvd(num_gpus=1)
+    import os
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    # os.environ["jax_enable_x64"] = "True"
+
+    from astronomix import time_integration
 import jax.numpy as jnp
 from jaxtyping import Array
 from astronomix.data_classes.simulation_helper_data import HelperData
@@ -13,10 +23,17 @@ from astronomix import (
     initialize_interface_fields,
 )
 
+from astronomix.data_classes.simulation_snapshot_data import SnapshotData
 from astronomix.option_classes.simulation_config import STATE_TYPE
 from astronomix.variable_registry.registered_variables import RegisteredVariables
 from arena.arena_tests.solver_in_the_loop.multiproblem.problems.baseproblem import (
     BaseProblem,
+)
+
+from arena.arena_tests.solver_in_the_loop.plot_states_comparison import (
+    plot_states,
+    plot_and_animate_states_with_diagonal,
+    animate_several_sims_states_with_diagonal,
 )
 
 import logging
@@ -157,3 +174,93 @@ def initial_state_mhd_blast(
 
     config = finalize_config(config, initial_state.shape)
     return (initial_state, config, params, helper_data, registered_variables)
+
+
+if __name__ == "__main__":
+    from arena.arena_tests.solver_in_the_loop.multiproblem.problem_manager import (
+        _build_hr_config_and_params,
+    )
+    from arena.arena_tests.solver_in_the_loop.model_manager import TrainingConfig
+
+    b0 = 0.2
+    r0 = 0.125
+    B_direction = jnp.array([1, 0, 0])
+    num_cells_hr = 64
+    num_snapshots = 50
+
+    training_config = TrainingConfig(epochs_per_time=[], snapshot_timepoints_train=[])
+
+    config, params = _build_hr_config_and_params(training_config=training_config)
+    # blast = MhdBlast(B_direction=B_direction, B0=b0)
+    #
+    # folder = f"results/mhd_blast/wo_fix/{blast.filename}"
+    # config_overrides = blast.get_config_overrides_evaluation(
+    #     snapshot_timepoints=jnp.linspace(0.0, 0.4, num=40, endpoint=True)
+    # )
+    #
+    # (initial_state, config, params, registered_variables) = (
+    #     blast.generate_initial_state_with_config_overrides(
+    #         config=config, params=params, config_overrides=config_overrides
+    #     )
+    # )
+    # plot_states(
+    #     states_list=[initial_state],
+    #     z_levels=[num_cells_hr // 2],
+    #     folder=folder,
+    #     fig_name=f"initial_final_state_{num_cells_hr}",
+    # )
+    # snapshot_data = time_integration(
+    #     primitive_state=initial_state,
+    #     config=config,
+    #     params=params,
+    #     registered_variables=registered_variables,
+    # )
+
+    # testing for b0 values
+    for b0 in jnp.linspace(1.1, 1.4, 10, endpoint=True):
+        blast = MhdBlast(B_direction=jnp.array([1, 0, 0]), B0=b0)
+        print(blast.B0)
+
+        config_overrides = blast.get_config_overrides_evaluation(
+            snapshot_timepoints=jnp.linspace(0.0, 0.4, num=40, endpoint=True)
+        )
+
+        (initial_state, config, params, registered_variables) = (
+            blast.generate_initial_state_with_config_overrides(
+                config=config, params=params, config_overrides=config_overrides
+            )
+        )
+        try:
+            snapshot_data = time_integration(
+                primitive_state=initial_state,
+                config=config,
+                params=params,
+                registered_variables=registered_variables,
+            )
+        except ValueError:
+            print(f"bo error value {b0}")
+            exit()
+        assert isinstance(snapshot_data, SnapshotData)
+    exit()
+
+    last_true_state = -1
+    for state in snapshot_data.states:
+        if jnp.any(state != 0.0):
+            last_true_state += 1
+
+    plot_states(
+        states_list=[initial_state, snapshot_data.states[last_true_state]],
+        z_levels=[num_cells_hr // 2, num_cells_hr // 2],
+        folder=folder,
+        fig_name=f"initial_final_state_{num_cells_hr}",
+    )
+
+    animate_several_sims_states_with_diagonal(
+        sim_states=[snapshot_data.states],
+        sim_legend=[f"{num_cells_hr}^3"],
+        z_levels=[num_cells_hr // 2],
+        timepoints=snapshot_data.time_points,
+        save_path=folder,
+        file_name=f"animation_{num_cells_hr}",
+        fps=5,
+    )
